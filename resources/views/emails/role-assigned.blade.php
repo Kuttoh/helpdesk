2be0787548ac499d3e2bd6a2d1212bcad776b07a
Kuttoh <kuttohisaac@gmail.com>
2019-08-08 07:51:50 +0300

    add 'role assigned by' in role-assigned email

--- resources/views/emails/role-assigned.blade.php
+++ resources/views/emails/role-assigned.blade.php
@@ -2,6 +2,8 @@
 
 You've been assigned a new role: <strong>{{ $user->role->name }}</strong>
 
+Assigned by: <strong>{{ auth()->user()->firstname }} {{ auth()->user()->lastname }}</strong>
+
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
